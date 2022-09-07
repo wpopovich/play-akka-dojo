613017e1a5b7093fd3d51687ce367bee0d9de310
Walter Popovich <walter.popovich@tiendanube.com>
2022-09-07 11:59:47 -0300

    Add student courses
    Signed-off-by: Walter Popovich <walter.popovich@tiendanube.com>

--- app/controllers/AkkaController.scala
+++ app/controllers/AkkaController.scala
@@ -2,17 +2,21 @@ package controllers
 
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.{ActorRef, ActorSystem, Props, Scheduler, SpawnProtocol}
+import akka.pattern.StatusReply
 import akka.util.Timeout
-import models.{CreateStudent, GetStudent, Student, StudentCommand, StudentState}
-import play.api.libs.json.Json
+import models.{CreateStudent, EnrollStudent, GetStudent, Student, StudentCommand, StudentState}
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.{JsPath, Json, Reads}
 import play.api.mvc
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 
 import java.util.UUID
+import java.util.concurrent.TimeoutException
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
 
 @Singleton
 class AkkaController @Inject()(val controllerComponents: ControllerComponents,
@@ -33,9 +37,53 @@ class AkkaController @Inject()(val controllerComponents: ControllerComponents,
       .map(Ok(_))
   }
 
+  case class EnrollStudentRequest(courses: Set[String])
+  implicit val t = Json.reads[EnrollStudentRequest]
+  def enrollStudent(id: String): Action[EnrollStudentRequest] = Action.async(parse.json[EnrollStudentRequest]) { request =>
+    val parsed : EnrollStudentRequest = request.body
+
+    val actorRef = getStudentActor(id)
+    actorRef.ask(replyTo => EnrollStudent(parsed.courses, replyTo))
+      .map(_ => NoContent)
+  }
+
   def getStudent(id: String): Action[AnyContent] = Action.async {
+    /*
+    $student = Student::find(id)
+    return $student.name
+    
+    $student = Student::find(id)
+    $student.enrollCourse($curso)
+     */
     val actOf = getStudentActor(id)
-    (actOf ? GetStudent).map(name => Ok(name))
+    actOf.ask[StatusReply[String]](GetStudent)
+      .map(student => Ok(student.getValue))
+      .recover {
+        case StatusReply.ErrorMessage(error) => NotFound(s"Stuent $id not found, ${error}")
+      }
+
+//      .onComplete {
+//        case Failure(ex) =>
+//          println(s"ex $ex")
+//          NotFound
+//        case Success(value) =>
+//          println("Hello")
+//          Ok(value)
+//      }
+
+//      .map(reply =>
+
+//      reply match {
+//        case a: Stat
+//      }
+//      if (reply.isSuccess)
+//        Ok(reply.getValue)
+//      else
+//        NotFound(reply.getValue)
+//      reply match {
+//      case StatusReply.Success(name) => Ok(name)
+//      case StatusReply.Error(error) => NotFound(error)
+//    }
   }
 
   def getStudentActor(id: String): ActorRef[StudentCommand] = {
--- app/models/Student.scala
+++ app/models/Student.scala
@@ -1,13 +1,15 @@
 package models
 
+import akka.Done
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.pattern.StatusReply
 import akka.persistence.Persistence
 import akka.persistence.typed.PersistenceId
-import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior, ReplyEffect}
 
 case class Student() {
-
+  
 }
 
 sealed trait StudentState {
@@ -21,6 +23,9 @@ case class EmptyStudent(id: String) extends StudentState {
       case CreateStudent(name, replyTo) =>
         Effect.persist(StudentCreated(name))
           .thenReply(replyTo)(_ => id)
+      case GetStudent(replyTo) =>
+        Effect.reply(replyTo)(StatusReply.Error("Id no existe capooo"))
+
     }
   }
 
@@ -28,18 +33,33 @@ case class EmptyStudent(id: String) extends StudentState {
     case StudentCreated(name) => CreatedStudent(id, name)
   }
 }
-case class CreatedStudent(id: String, name: String) extends StudentState {
-  override def commandHandler(command: StudentCommand): Effect[StudentEvent, StudentState] = ???
+case class CreatedStudent(id: String, name: String, courses: Set[String] = Set.empty) extends StudentState {
+  override def commandHandler(command: StudentCommand): Effect[StudentEvent, StudentState] = {
+    command match {
+      case GetStudent(replyTo) =>
+        Effect.reply(replyTo)(StatusReply.Success(name))
+      case EnrollStudent(courses, replyTo) =>
+        Effect.persist(StudentEnrolled(courses))
+          .thenReply(replyTo)(_ => StatusReply.Ack)
+    }
+  }
 
-  override def eventHandler(event: StudentEvent): StudentState = ???
+  //TODO: Handle StudentEnrolled
+  override def eventHandler(event: StudentEvent): StudentState = event match {
+    case StudentEnrolled(newCourses) => 
+      copy(courses = courses ++ newCourses)
+  }
 }
 
 sealed trait StudentCommand
 case class CreateStudent(name:String, replyTo: ActorRef[String]) extends StudentCommand
-case class GetStudent(replyTo: ActorRef[String]) extends StudentCommand
+case class GetStudent(whatever: ActorRef[StatusReply[String]]) extends StudentCommand
+case class EnrollStudent(courses: Set[String], replyTo: ActorRef[StatusReply[Done]]) extends StudentCommand
 
 sealed trait StudentEvent
 case class StudentCreated(name: String) extends StudentEvent
+case class StudentEnrolled(courses: Set[String]) extends StudentEvent
+
 
 object Student {
 
